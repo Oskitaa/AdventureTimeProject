df2c58d551e3ee9f501065fb6ee3855191550743
ehurjus <77842895+ehurjus@users.noreply.github.com>
2021-02-12 14:48:06 +0100

    Update angular project

--- AdventureTimes/src/app/Components/escenarios/escenarios.component.ts
+++ AdventureTimes/src/app/Components/escenarios/escenarios.component.ts
@@ -5,7 +5,7 @@ import {NgbCarousel, NgbSlide, NgbSlideEvent, NgbSlideEventSource} from '@ng-boo
 @Component({
   selector: 'app-escenarios',
   templateUrl: './escenarios.component.html',
-  styleUrls: ['./escenarios.component.sass']
+  styleUrls: ['./escenarios.component.scss']
 })
 export class EscenariosComponent implements OnInit {
 
@@ -14,7 +14,12 @@ export class EscenariosComponent implements OnInit {
   ngOnInit(): void {
   }
   
-    public images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
+    public images = [
+      "https://i.pinimg.com/originals/65/8d/50/658d505bd8b224f8f2dd6ec23c0f2e8c.jpg",
+      "https://pm1.narvii.com/7619/6785c4e08ca1c0ea1f357c096d98ec38021340d7r5-1600-900_00.jpg",
+      "https://www.lacasadeel.net/wp-content/uploads/2015/03/reino-de-hielo-hora-de-aventuras.jpg",
+      "https://i.pinimg.com/originals/a4/f2/8f/a4f28f895afc3116485c0f9489941815.jpg"
+    ];
 
     paused = false;
     unpauseOnArrow = false;
@@ -23,7 +28,7 @@ export class EscenariosComponent implements OnInit {
     pauseOnFocus = true;
 
     @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
-
+    
     togglePaused() {
       if (this.paused) {
         this.carousel.cycle();
@@ -31,16 +36,6 @@ export class EscenariosComponent implements OnInit {
         this.carousel.pause();
       }
       this.paused = !this.paused;
-    }
-
-    onSlide(slideEvent: NgbSlideEvent) {
-      if (this.unpauseOnArrow && slideEvent.paused &&
-        (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
-        this.togglePaused();
-      }
-      if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
-        this.togglePaused();
-      }
     }
   }
 

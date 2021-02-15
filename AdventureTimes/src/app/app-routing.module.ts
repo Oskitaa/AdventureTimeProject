ca0a32b24f065b13d37e6e7499026f731a0f202d
jmferdia <jmferreiradiaz@gmail.com>
2021-02-15 14:25:47 +0100

    falta tamaño movil

--- AdventureTimes/src/app/app-routing.module.ts
+++ AdventureTimes/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EpisodiosComponent } from './Components/episodios/episodios.component';
+import { EscenariosComponent } from './Components/escenarios/escenarios.component';
 import { InicioComponent } from './Components/inicio/inicio.component';
+import { TiendaComponent } from './Components/tienda/tienda.component';
 
 const routes: Routes = [
   {
     path:"",
     component:InicioComponent
+  },
+  {
+    path:"escenarios",
+    component:EscenariosComponent
+  },
+  {
+    path:"episodios",
+    component:EpisodiosComponent
+  },
+  {
+    path:"tienda",
+    component:TiendaComponent
   }
 ];
 

bd734f3e080b00db1d11b610315b870c5d4553f1
Chirili <chirili503@gmail.com>
2021-02-18 23:57:22 +0100

    Update agregado un fichero javascript que exporta un observable y modificado el componente info-escenarios

--- AdventureTimes/src/app/Components/escenarios/info-escenarios/info-escenarios.component.ts
+++ AdventureTimes/src/app/Components/escenarios/info-escenarios/info-escenarios.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
+
+import {windowResizeObservable} from 'src/assets/refreshAOS.js';
+
 @Component({
   selector: 'app-info-escenarios',
   templateUrl: './info-escenarios.component.html',
   styleUrls: ['./info-escenarios.component.scss']
 })
 export class InfoEscenariosComponent implements OnInit {
-  
+  defaultWidth = 1920
   constructor() { }
 
-  refreshAOS(event){
-    console.log(event);
-    AOS.refresh();
-  }
   ngOnInit(): void {
-    AOS.init();
+    windowResizeObservable.subscribe((data) => {
+      console.log(data);
+      AOS.refreshHard()
+    })
+    AOS.init({
+    });
   }
-
+  ngOnDestroy(){
+    windowResizeObservable.unsubscribe();
+  }
 }

9d5d5bc06093347993db592c999a7d238fef9d9d
Oskitaa <ese.betico.98@gmail.com>
2021-02-23 10:21:14 +0100

    Peticiones ajax y primer modelo de diseño

--- AdventureTimes/src/app/app-routing.module.ts
+++ AdventureTimes/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EpisodeDetailsComponent } from './Components/episode-details/episode-details.component';
+import { EpisodiosComponent } from './Components/episodios/episodios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: "seasons",
+  component: EpisodiosComponent
+  },
+  {
+    path: "seasons/:id",
+    component: EpisodeDetailsComponent
+  },
+  {
+    path: "seasons/:idSeason/episode/:idEpisode",
+    component: EpisodeDetailsComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
